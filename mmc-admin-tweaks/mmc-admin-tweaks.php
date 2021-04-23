448de0b7c8dc506057205f62f512d915aa89199b
Melissa Cabral <melissacabral@gmail.com>
2021-04-23 11:05:07 -0700

    added dashboard widgets example to admin tweaks plugin

--- mmc-admin-tweaks/mmc-admin-tweaks.php
+++ mmc-admin-tweaks/mmc-admin-tweaks.php
@@ -53,15 +53,18 @@ function mmc_tweak_toolbar( $wp_admin_bar ){
 	) );
 }
 
-//add or remove a page to the admin panel
-add_action('admin_menu', 'mmc_help_page');
-function mmc_help_page(){
+/**
+ * add and remove pages from  the admin panel
+ */
+add_action('admin_menu', 'mmc_admin_menu');
+function mmc_admin_menu(){
+	//add a page under "dashboard"
 					//  page title 			menu title 		capability 	 menu_slug 			callback 		position
 	add_dashboard_page( 'Helpful Resources', 'Resources', 'edit_posts', 'mmc_resources', 'mmc_resources_page', 10 );
 
-	//remove comments from the menum (only if not an administrator)
+	//remove comments from the menu (only if not an administrator)
 	if( ! current_user_can('manage_options') ){
-		remove_menu_page( 'edit-comments.php' );          //Comments
+		remove_menu_page( 'edit-comments.php' );   
 	}
 }
 
@@ -72,3 +75,40 @@ function mmc_resources_page(){
 	</div>
 	<?php
 }
+
+
+/**
+ * Remove Dashboard widgets
+ */
+add_action( 'admin_menu', 'mmc_dashboard' );
+function mmc_dashboard(){
+	//detect what type of user (manage options checks for administrator)
+	if( current_user_can('manage_options') ){
+		//remove quick press for admins
+		remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+	}else{
+		//remove news and events for every other type of user
+		remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+	}	
+
+	
+}
+
+/**
+ * Add dashboard widgets
+ */
+add_action( 'wp_dashboard_setup', 'mmc_dash_widget_add' );
+function mmc_dash_widget_add(){
+	wp_add_dashboard_widget( 'mmc_support', 'Helpful Videos', 'mmc_custom_dash_widget' );
+}
+
+
+//custom callback for the content of the widget
+function mmc_custom_dash_widget(){
+	?>
+
+	<iframe width="300" height="169" src="https://www.youtube.com/embed/x7R2HcTAyjI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+	<?php
+}
+
